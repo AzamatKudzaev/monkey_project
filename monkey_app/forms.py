7f05021ea9a9040e08030109f725148c74a23d16
azamat <azamat@oleg.eg>
2023-03-10 14:07:35 +0600

    forms fix

--- monkey_app/forms.py
+++ monkey_app/forms.py
@@ -3,29 +3,6 @@ from .models import Article, Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# class ArticleForm(forms.Form):
-
-#     NOT_SELECTED = 'not selected'
-#     SCINEFIC = 'scinefic'
-#     REVIEW = 'review'
-#     INFORMATIONAL = 'informational'
-
-#     ARTICLES_KINDS = {
-#         (NOT_SELECTED, 'Not selected'),
-#         (SCINEFIC, 'Scientific'),
-#         (REVIEW, 'Review'),
-#         (INFORMATIONAL, 'Informational')
-#     }
-
-#     title = forms.CharField(label="Заголовок",min_length=10, max_length=50, error_messages={
-#         'min_lenght': 'Слишком мало символов',
-#         'required': 'поле обязательное'
-#     })
-#     text = forms.CharField(label="Основной текст", max_length=500, min_length=50,widget=forms.Textarea(attrs={'rows':20, 'cols':150}))
-#     image = forms.ImageField(label="Фото", required=False)
-#     article_kind = forms.ChoiceField(label="Тип статьи", choices=ARTICLES_KINDS)
-
-
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
